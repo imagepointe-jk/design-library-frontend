ef958c390afac8f5f2657316ec1113dfdecd329b
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2023-11-30 12:16:31 -0600

    Add preliminary setup for filter modal

--- src/components/DesignLibrary.tsx
+++ src/components/DesignLibrary.tsx
@@ -5,15 +5,17 @@ import { useState, useEffect } from "react";
 import { TempDesignWithImage } from "../sharedTypes";
 import { getDesigns } from "../fetch";
 import styles from "./styles/DesignLibrary.module.css";
+import { FilterModal } from "./FilterModal";
 
 export function DesignLibrary() {
   const { designId: designIdStr } = useParams();
   const [searchParams] = useSearchParams();
-
-  const designId = designIdStr !== undefined ? +designIdStr : 0;
   const [designs, setDesigns] = useState<TempDesignWithImage[] | undefined>(
     undefined
   );
+  const [showFilterModal, setShowFilterModal] = useState(false);
+
+  const designId = designIdStr !== undefined ? +designIdStr : 0;
 
   async function getDesignsToDisplay() {
     try {
@@ -45,54 +47,65 @@ export function DesignLibrary() {
   ];
 
   return (
-    <div className={styles["bg"]}>
-      <div className="inner-body">
-        <p className={styles["library-text"]}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-          unde, provident omnis libero corporis minus, voluptas perspiciatis
-          deserunt, natus eos eligendi. Impedit veritatis placeat dignissimos
-          perferendis possimus distinctio, eos eum!
-        </p>
-        <div className={styles["search-container"]}>
-          <div className={styles["settings-container"]}>
-            <div className={styles["settings-subcontainer"]}>
-              <label htmlFor="screen-print">
-                <input type="radio" name="design-type" id="screen-print" />
-                Screen Print
-              </label>
-              <label htmlFor="embroidery">
-                <input type="radio" name="design-type" id="embroidery" />
-                Embroidery
-              </label>
+    <>
+      <div className={styles["bg"]}>
+        <div className="inner-body">
+          <p className={styles["library-text"]}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            unde, provident omnis libero corporis minus, voluptas perspiciatis
+            deserunt, natus eos eligendi. Impedit veritatis placeat dignissimos
+            perferendis possimus distinctio, eos eum!
+          </p>
+          <div className={styles["search-container"]}>
+            <div className={styles["settings-container"]}>
+              <div className={styles["settings-subcontainer"]}>
+                <label htmlFor="screen-print">
+                  <input type="radio" name="design-type" id="screen-print" />
+                  Screen Print
+                </label>
+                <label htmlFor="embroidery">
+                  <input type="radio" name="design-type" id="embroidery" />
+                  Embroidery
+                </label>
+              </div>
+              <div className={styles["settings-subcontainer"]}>
+                {checkboxButtons.map((button) => (
+                  <>
+                    <input
+                      className="button-styled-checkbox"
+                      type="checkbox"
+                      name={button.name}
+                      id={button.name}
+                    />
+                    <label htmlFor={button.name}>{button.label}</label>
+                  </>
+                ))}
+                <button
+                  className={styles["settings-button"]}
+                  onClick={() => {
+                    console.log("click");
+                    setShowFilterModal(true);
+                  }}
+                >
+                  <i className="fa-solid fa-sliders"></i>
+                  Filters
+                </button>
+                <button className={styles["settings-button"]}>
+                  <i className="fa-solid fa-magnifying-glass"></i>
+                  Search
+                </button>
+              </div>
             </div>
-            <div className={styles["settings-subcontainer"]}>
-              {checkboxButtons.map((button) => (
-                <>
-                  <input
-                    className="button-styled-checkbox"
-                    type="checkbox"
-                    name={button.name}
-                    id={button.name}
-                  />
-                  <label htmlFor={button.name}>{button.label}</label>
-                </>
-              ))}
-              <button className={styles["settings-button"]}>
-                <i className="fa-solid fa-sliders"></i>
-                Filters
-              </button>
-              <button className={styles["settings-button"]}>
-                <i className="fa-solid fa-magnifying-glass"></i>
-                Search
-              </button>
-            </div>
+            {designs && <DesignGrid designs={designs} />}
+            {designId !== undefined && designId > 0 && (
+              <DesignModal designId={designId} />
+            )}
           </div>
-          {designs && <DesignGrid designs={designs} />}
-          {designId !== undefined && designId > 0 && (
-            <DesignModal designId={designId} />
-          )}
         </div>
       </div>
-    </div>
+      {showFilterModal && (
+        <FilterModal clickAwayFunction={() => setShowFilterModal(false)} />
+      )}
+    </>
   );
 }
---
+++ src/components/FilterModal.tsx
@@ -0,0 +1,94 @@
+import { Modal } from "./Modal";
+import styles from "./styles/FilterModal.module.css";
+
+type FilterModalProps = {
+  clickAwayFunction: () => void;
+};
+
+export function FilterModal({ clickAwayFunction }: FilterModalProps) {
+  const mainCategories = [
+    {
+      label: "Quick Search",
+      name: "quick-search",
+    },
+    {
+      label: "Event/Awareness",
+      name: "event-awareness",
+    },
+    {
+      label: "Union Specific",
+      name: "union-specific",
+    },
+    {
+      label: "Holidays",
+      name: "holidays",
+    },
+    {
+      label: "Location Specific",
+      name: "location-specific",
+    },
+  ];
+
+  const subcategories = [
+    {
+      label: "New Designs",
+      name: "new-designs",
+    },
+    {
+      label: "Best Sellers",
+      name: "best-sellers",
+    },
+    {
+      label: "Staff Favorites",
+      name: "staff-favorites",
+    },
+    {
+      label: "Classics",
+      name: "classics",
+    },
+    {
+      label: "Patriotic",
+      name: "patriotic",
+    },
+    {
+      label: "Solidarity",
+      name: "solidarity",
+    },
+    {
+      label: "Economy",
+      name: "economy",
+    },
+    {
+      label: "Ladies",
+      name: "ladies",
+    },
+    {
+      label: "All Designs",
+      name: "all-designs",
+    },
+  ];
+
+  return (
+    <Modal clickAwayFunction={clickAwayFunction}>
+      <h2>Screen Print Design Library Filters</h2>
+      <p>Select a main category on the right and a subcategory below</p>
+      <div className={styles["main-flex"]}>
+        <div>
+          {mainCategories.map((category) => (
+            <div>
+              <input
+                className="button-styled-checkbox"
+                type="checkbox"
+                name={category.name}
+                id={category.name}
+              />
+              <label htmlFor={category.name}>{category.label}</label>
+            </div>
+          ))}
+          <button>Apply Filters</button>
+          <button>Clear Selection</button>
+        </div>
+      </div>
+    </Modal>
+  );
+}

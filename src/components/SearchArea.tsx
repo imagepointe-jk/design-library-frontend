d6d65684843dfab1722bb71f47ad0ae266dc433f
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2023-12-15 12:56:19 -0600

    Restore search functionality

--- src/components/SearchArea.tsx
+++ src/components/SearchArea.tsx
@@ -1,15 +1,64 @@
+import { DesignType, designTypes } from "../sharedTypes";
+import { DesignQueryParams } from "../types";
+import {
+  buildDesignQueryParams,
+  requestParentWindowUrlChange,
+} from "../utility";
+import { useApp } from "./AppProvider";
 import styles from "./styles/SearchArea.module.css";
 
 export function SearchArea() {
+  const { parentWindowLocation } = useApp();
+
+  function submitSearch(e: React.FormEvent<HTMLFormElement>) {
+    if (!parentWindowLocation) return;
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const keywords = formData.get("search");
+    const designType = formData.get("design-type");
+    const newParams: DesignQueryParams = {
+      designType: "Screen Print",
+      pageNumber: 1,
+      keywords: keywords?.toString().split(" "),
+      featuredOnly: true,
+    };
+    if ((designType as DesignType) === "Embroidery")
+      newParams.designType = "Embroidery";
+
+    requestParentWindowUrlChange(
+      `${
+        parentWindowLocation.origin
+      }/design-library-new-designs/?${buildDesignQueryParams(newParams)}`
+    );
+  }
+
   return (
-    <div className={styles["search-row"]}>
-      <input
-        type="search"
-        placeholder="Search For A Design"
-        name="search"
-        id="search"
-      />
-      <button>Search</button>
-    </div>
+    <form onSubmit={submitSearch}>
+      <div className={styles["search-row"]}>
+        <input
+          type="search"
+          placeholder="Search For A Design"
+          name="search"
+          id="search"
+        />
+        <button type="submit">Search</button>
+      </div>
+      <div className={styles["radio-buttons-container"]}>
+        {designTypes.map((designType) => (
+          <label htmlFor={`search-modal-${designType}`}>
+            <input
+              type="radio"
+              name={"design-type"}
+              id={`search-modal-${designType}`}
+              defaultChecked={designType === "Screen Print"}
+              value={designType}
+            />
+            {designType}
+          </label>
+        ))}
+      </div>
+    </form>
   );
 }

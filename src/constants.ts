107f0ed8443b217dfe7e10e3660306be8bbac334
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-01-22 15:04:34 -0600

    Fix page size choices

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const defaultModalHeight = 700,
   searchModalHeight = 250,
   searchModalMaxWidth = 550,
-  pageSizeChoices = [3, 5, 7],
+  pageSizeChoices = [18, 30, 50],
   //there are lots of factors that determine when (if ever) the iframe's parent window will respond.
   //wait for this long before assuming something went wrong and showing the user an error message.
   waitForParentMs = 4000;

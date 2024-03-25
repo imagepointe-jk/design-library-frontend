8e5b7120e069a7ce62f44cece15323cce799b48e
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-03-25 15:55:50 -0500

    Update spinner asset path; remove unused iframe-related constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,6 @@
-export const defaultModalHeight = 725,
-  searchModalHeight = 250,
-  searchModalMaxWidth = 550,
-  pageSizeChoices = [18, 30, 50],
-  //there are lots of factors that determine when (if ever) the iframe's parent window will respond.
-  //wait for this long before assuming something went wrong and showing the user an error message.
-  waitForParentMs = 4000,
-  productionPathName = "design-library-new-designs",
-  developmentPathName = "design-library-development";
+export const assetPaths = {
+  spinner1:
+    "https://www.imagepointe.com/wp-content/uploads/2024/03/spinner1.png",
+};
+
+export const pageSizeChoices = [18, 30, 50];

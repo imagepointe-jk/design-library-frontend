60fcf8d45d934312d496ca769d4c96061bbab6b6
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-03-28 14:20:50 -0500

    Tweak appearance (mimic mistaken main commit 1cc633e)

--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -88,6 +88,18 @@ export function TopSection() {
 
   return (
     <div className={styles["main"]}>
+      <div className={styles["hero-text"]}>
+        <h1>
+          <span className={styles["blue"]}>Union Designs</span> for Every
+          Occasion
+        </h1>
+        <p>
+          Unite your union in style by finding the perfect design from our
+          collection created in-house by our Art team. Choose from the screen
+          print and embroidery libraries with options to customize with your
+          colors, union and local.
+        </p>
+      </div>
       <SearchArea />
       <div className={styles["featured-image-container"]}>
         <NodeScrollView nodes={cards} isLoading={featuredDesignsLoading} />

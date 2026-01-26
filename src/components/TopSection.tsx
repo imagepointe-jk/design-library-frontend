9e9aeb5bad854c0200ca2f2a3dc99ab801f72110
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2026-01-26 14:23:58 -0600

    Add temp St. Patrick's Day link

--- src/components/TopSection.tsx
+++ src/components/TopSection.tsx
@@ -2,8 +2,11 @@ import styles from "./styles/TopSection.module.css";
 
 export function TopSection() {
   return (
-    <div className={styles["main"]}>
-      <div className={styles["hero-text"]}>
+    <div
+      className={styles["main"]}
+      style={{ flexDirection: "row", justifyContent: "center" }}
+    >
+      <div className={styles["hero-text"]} style={{ maxWidth: "850px" }}>
         <h1>
           <span className={styles["blue"]}>Union Designs</span> for Every
           Occasion
@@ -15,6 +18,31 @@ export function TopSection() {
           colors, union and local.
         </p>
       </div>
+      <a
+        href={`${window.location.origin}/?subcategory=${encodeURIComponent("St. Patrick's Day")}`}
+        style={{
+          background: "none",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "15px",
+        }}
+      >
+        <img
+          src="https://imagepointe.com/wp-content/uploads/2026/01/St-Patricks-Day-600-x-600-px.jpg"
+          style={{ width: "300px" }}
+        />
+        <div
+          style={{
+            backgroundColor: "#2cc922",
+            padding: "15px 20px",
+            borderRadius: "10px",
+            fontWeight: "600",
+          }}
+        >
+          View Designs
+        </div>
+      </a>
     </div>
   );
 }
